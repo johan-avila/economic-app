582c388f366fe9d9d95dc8afd153293204df259d
johan-avila <johan-avila@outlook.com>
2020-11-20 22:45:29 -0600

    Before filling database

--- data/index.js
+++ data/index.js
@@ -1,18 +1,41 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
+
+// const conect =  mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "crud_nest"
+//   })
+
+//   conect.connect((err)=> {
+//     if (err) throw err;
+//     // -----------------
+//     console.log("Connected!");
+//     var sql = `INSERT INTO posts (title, category, tags, status)
+//                  VALUES ('First title', 'TECH' , '["Hola", "mundo"]', true)`
+//     conect.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("1 record inserted", result);
+//     });
+//   });
+
+// ----------------------------
 const fs = require("fs");
-fs.readFile(`${__dirname}/csv/total_por_estado.csv`, "utf-8" ,async (err, data)=>{
+fs.readFile(`${__dirname}/csv/secundarias.csv`, "utf-8" ,async (err, data)=>{
     
     let arr = await data.split(",")
     
     let conceptos = []
     for (let i = 0; i < data.length; i++) {
         if(arr[i*12]){
-            conceptos.push(arr[i*12])
+            if(true){
+              conceptos.push(arr[i*12])
+            }
             
         }    
     }
-    console.log(conceptos.length);
-    conceptos.forEach(e => console.log(e))
+    console.table(conceptos);
+    // conceptos.forEach(e => console.(e))
 
 })
 
